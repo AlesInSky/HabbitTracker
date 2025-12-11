37d4f7401b1380fea3fb2bcec77002223a13a8bf
Eugene <rezak97@mail.ru>
2025-12-11 21:38:28 +0300

    Перенос CalendarViewModel в отдельную папку

--- app/src/main/java/com/example/habbittracker/di/AppModule.kt
+++ app/src/main/java/com/example/habbittracker/di/AppModule.kt
@@ -1,6 +1,6 @@
 package com.example.habbittracker.di
 
-import com.example.habbittracker.presentation.CalendarViewModel
+import com.example.habbittracker.presentation.viewmodel.CalendarViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
