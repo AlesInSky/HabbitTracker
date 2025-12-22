66e4345bc96bd13d27a9222a16296c6371498edb
Eugene <rezak97@mail.ru>
2025-12-22 23:45:28 +0300

    Черновой DAO

---
+++ data/src/main/java/com/example/data/local/dao/CalendarDao.kt
@@ -0,0 +1,27 @@
+package com.example.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.data.local.entity.CalendarEntity
+
+@Dao
+interface CalendarDao {
+
+    @Insert
+    suspend fun insert(habit: CalendarEntity)
+
+    @Update
+    suspend fun update(habit: CalendarEntity)
+
+    @Delete
+    suspend fun delete(habit: CalendarEntity)
+
+    @Query("SELECT * FROM calendar_habits WHERE calendar_date = :calendarDate")
+    suspend fun getFromDate(calendarDate: Int): List<CalendarEntity?>
+
+    @Query("SELECT * FROM calendar_habits ORDER BY calendarId DESC")
+    fun getAll(): List<CalendarEntity>
+}
