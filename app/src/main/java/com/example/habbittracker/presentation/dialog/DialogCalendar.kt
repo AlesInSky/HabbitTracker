d4749df448c63accfd9864e0df74923779d2ebad
Eugene <rezak97@mail.ru>
2025-12-22 23:22:32 +0300

    Перенос в другую папку

--- app/src/main/java/com/example/habbittracker/presentation/dialog/DialogCalendar.kt
+++ app/src/main/java/com/example/habbittracker/presentation/dialog/DialogCalendar.kt
@@ -1,4 +1,4 @@
-package com.example.habbittracker.presentation
+package com.example.habbittracker.presentation.dialog
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
