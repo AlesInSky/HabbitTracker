f2fb81217c70ed091bbe86efb5434ad170c25966
Eugene <rezak97@mail.ru>
2025-12-22 23:24:15 +0300

    Добавлены habitUnit(единица измерения) и habitQuantity

--- data/src/main/java/com/example/data/local/entity/HabitEntity.kt
+++ data/src/main/java/com/example/data/local/entity/HabitEntity.kt
@@ -5,11 +5,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "habits")
-data class HabitEntity (
+data class HabitEntity(
     @PrimaryKey(autoGenerate = true)
     var habitId: Long = 0L,
     @ColumnInfo(name = "habit_name")
     var habitName: String = "",
+    @ColumnInfo(name = "habit_unit")
+    var habitUnit: String = "",
+    @ColumnInfo(name = "habit_quantity")
+    var habitQuantity: Int = 0,
     @ColumnInfo(name = "habit_description")
     var habitDescription: String = "",
     @ColumnInfo(name = "habit_image")
