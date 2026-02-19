64bd3284a8252fef49f316ca7cf7953b7ef357b2
Eugene <rezak97@mail.ru>
2026-02-19 19:40:57 +0300

    Добавлен комментарий

--- app/src/main/java/com/example/habbittracker/presentation/dialog/DialogCalendar.kt
+++ app/src/main/java/com/example/habbittracker/presentation/dialog/DialogCalendar.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-
+//Диалоговое окно при нажатии на дату в календаре
 @Composable
 fun DialogHabbit(
     onDismissRequest: () -> Unit,
