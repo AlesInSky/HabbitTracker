05a65bf03f0706c2f9fe955c307bd80d633751f0
Eugene <rezak97@mail.ru>
2026-02-21 15:19:15 +0300

    Черновой вариант окна добавления привычки

---
+++ app/src/main/java/com/example/habbittracker/presentation/dialog/DialogNewCalendarHabit.kt
@@ -0,0 +1,162 @@
+package com.example.habbittracker.presentation.dialog
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.data.local.entity.HabitEntity
+import com.example.habbittracker.R
+import com.example.habbittracker.presentation.viewmodel.CalendarViewModel
+
+//Диалоговое окно при добавлении привычки в дату календаря
+@Composable
+fun DialogNewCalendarHabit(
+    habit: HabitEntity?,
+    onDismissRequest: () -> Unit,
+    onSave: (HabitEntity) -> Unit,
+    vm: CalendarViewModel,
+) {
+    // Если нет привычки для редактирования - не показываем диалог
+    if (habit == null) return
+
+    // Локальные состояния для редактирования
+    var name by remember { mutableStateOf(habit.habitName) }
+    var description by remember { mutableStateOf(habit.habitDescription) }
+
+
+
+    // Для DropDown Menu
+    var expanded by remember { mutableStateOf(false) }
+    var selectedUnit by remember { mutableStateOf<HabitEntity?>(null) }
+    val habitLoad by vm.habitList.collectAsState()
+
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)  // Больше места для содержимого
+                .padding(10.dp),
+            shape = MaterialTheme.shapes.large,
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                // Заголовок
+                Text(
+                    text = "Добавить привычку",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    // Список иконок
+                    IconButton(
+                        onClick = { /*selectedImage = true*/ }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.card_add_icon),
+                            contentDescription = "Выбор иконки",
+                            tint = Color.Unspecified,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+
+                    Button(
+                        onClick = { expanded = true },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text(text = selectedUnit?.habitName ?: "Выберите привычку")
+                    }
+
+                    // Поле для названия
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        habitLoad.forEach { habit ->
+                            DropdownMenuItem(
+                                text = { Text(habit.habitName) },
+                                onClick = {
+                                    selectedUnit = habit
+                                    expanded = false
+                                    name = habit.habitName
+                                })
+                        }
+                    }
+                }
+
+
+
+                // Поле для комментария
+                OutlinedTextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = { Text("Комментарий") },
+                    modifier = Modifier.fillMaxWidth(),
+                    minLines = 2
+                )
+
+                // Кнопки действий
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+//                        TextButton(
+//                            onClick = onDismissRequest,
+//                            modifier = Modifier.padding(end = 8.dp)
+//                        ) {
+//                            Text("Отмена")
+//                        }
+
+                    Button(
+                        onClick = {
+                            // Создаем обновленную привычку
+                            val updatedHabit = habit.copy(
+                                habitName = name,
+                                habitDescription = description,
+                                habitImage = R.drawable.card_add_icon
+                            )
+                            onSave(updatedHabit)
+                        }, modifier = Modifier.fillMaxSize()
+                    ) {
+                        Text("Сохранить")
+                    }
+                }
+            }
+        }
+    }
+}
