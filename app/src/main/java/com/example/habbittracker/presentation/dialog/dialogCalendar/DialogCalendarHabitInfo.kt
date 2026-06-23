89716d1fd07037ff81fcfa14d9764501b8867c57
Eugene <rezak97@mail.ru>
2026-06-23 20:59:41 +0300

    Исправлена высота окна (Теперь динамическая высота). Сумма и количество при целом числе не отображаются с точкой.

--- app/src/main/java/com/example/habbittracker/presentation/dialog/dialogCalendar/DialogCalendarHabitInfo.kt
+++ app/src/main/java/com/example/habbittracker/presentation/dialog/dialogCalendar/DialogCalendarHabitInfo.kt
@@ -1,7 +1,6 @@
 package com.example.habbittracker.presentation.dialog.dialogCalendar
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
