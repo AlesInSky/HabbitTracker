72ccc1ea2498895423adaeda7a20c1d59521320f
Eugene <rezak97@mail.ru>
2026-06-23 21:23:31 +0300

    implementation Swipe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.swipe2)
 
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
